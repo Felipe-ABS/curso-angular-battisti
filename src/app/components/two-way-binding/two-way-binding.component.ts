6bf5b9b7ebc4894b0dfc8024860d30d489f8b47a
Felipe-ABS <FelipeABS2@Gmail.com>
2023-08-29 19:43:00 -0300

    15 - Two way data binding no Angular

---
+++ src/app/components/two-way-binding/two-way-binding.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-binding',
+  templateUrl: './two-way-binding.component.html',
+  styleUrls: ['./two-way-binding.component.css']
+})
+export class TwoWayBindingComponent implements OnInit {
+  name: string = '';
+  submittedName: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submitName() : void {
+    this.submittedName = this.name;
+  }
+
+}
